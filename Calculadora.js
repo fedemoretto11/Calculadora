945cf89c1ee7b611208ed366966ce773c33034ad
fedemoretto11 <fedemoretto94@gmail.com>
2023-01-09 23:01:09 -0300

    Se agrego el archivo clase de calculador para posterior modificacion

---
+++ Calculadora.js
@@ -0,0 +1,19 @@
+class Calculadora {
+
+    sumar() {
+        return num1 + num2
+    }
+
+    restar() {
+        return num1 - num2
+    }
+
+    multiplicar() {
+        return num1 * num2
+    }
+
+    dividir() {
+        return num1 / num2
+    }
+
+}
